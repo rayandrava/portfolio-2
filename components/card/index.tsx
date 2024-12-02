33d4146220399c9912fc7e51fbdc5827036a4d83
Rayandra Valera <rayandra.valera@gmail.com>
2024-12-02 10:14:45 +0700

    fix card typescript error

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -1,4 +1,11 @@
-export const Card = ({ title, body, imageSrc, cardClassName }) => (
+interface CardProps {
+  title: string;
+  body: string;
+  imageSrc: string;
+  cardClassName?: string;
+}
+
+export const Card = ({ title, body, imageSrc, cardClassName }: CardProps) => (
     <div className={`relative flex flex-col items-start gap-1 rounded-xl min-h-60 bg-gray-3 shadow-sm hover:decoration-none p-4 overflow-hidden ${cardClassName}`}>
         <div className="flex flex-col items-start md:gap-2 gap-1 z-50 md:max-w-[70%] w-full">
             <h3 className="hover:decoration-none">{title}</h3>
