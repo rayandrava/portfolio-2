10e2c60fb2981141fd57b4c148dd03c61fe76078
Rayandra Valera <rayandra.valera@gmail.com>
2024-12-02 10:18:27 +0700

    FIX ERROR in card

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -1,11 +1,11 @@
 interface CardProps {
   title: string;
-  body: string;
+  body?: string;
   imageSrc: string;
   cardClassName?: string;
 }
 
-export const Card = ({ title, body, imageSrc, cardClassName }: CardProps) => (
+export const Card = ({ title, body = "", imageSrc, cardClassName }: CardProps) => (
     <div className={`relative flex flex-col items-start gap-1 rounded-xl min-h-60 bg-gray-3 shadow-sm hover:decoration-none p-4 overflow-hidden ${cardClassName}`}>
         <div className="flex flex-col items-start md:gap-2 gap-1 z-50 md:max-w-[70%] w-full">
             <h3 className="hover:decoration-none">{title}</h3>
@@ -16,8 +16,6 @@ export const Card = ({ title, body, imageSrc, cardClassName }: CardProps) => (
             src={imageSrc} 
             alt={title} 
             className="image-3d"
-            // className="absolute top-16 left-32 transform translate-z-20 transition-all duration-300 hover:top-14"
-            // className={`${imageClassName}`} 
         />
         </div>
     </div>
